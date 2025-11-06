48d756f5b762734a67a2ff36791c836ab577348d
Mint <ozz.gaming@proton.me>
2025-11-06 05:47:40 +0100

    adjusted rules

--- keyboards/crkbd/keymaps/ecribia/rules.mk
+++ keyboards/crkbd/keymaps/ecribia/rules.mk
@@ -5,13 +5,13 @@ LTO_ENABLE          = yes
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = yes
-OLED_ENABLE         = no
+OLED_ENABLE         = yes
 OLED_DRIVER         = ssd1306
 EXTRAKEY_ENABLE     = no
-COMBO_ENABLE        = no
+COMBO_ENABLE        = yes
 
 QMK_SETTINGS        = yes
 
-CAPS_WORD_ENABLE = no
+CAPS_WORD_ENABLE = yes
 LAYER_LOCK_ENABLE = no
 REPEAT_KEY_ENABLE = no
